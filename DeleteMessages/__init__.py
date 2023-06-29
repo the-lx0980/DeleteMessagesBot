707a53ccd51a6be65b6cf22e32baf2e744a4f162
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-29 22:05:26 +0530

    Create __init__.py

---
+++ DeleteMessages/__init__.py
@@ -0,0 +1,32 @@
+# (c) Lx 0980
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+API_HASH = os.environ.get("API_HASH")
+APP_ID = int(os.environ.get("APP_ID"))
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_USER_SESSION = os.environ.get("TG_USER_SESSION")
+AUTH_USERS = 5326801541
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            "DeleteMessagesBot.txt",
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    """ get a Logger object """
+    return logging.getLogger(name)
