5168176a1fc3560403e739b1a957f239c7340ed7
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-29 22:33:48 +0530

    Create bot_messages.py

---
+++ DeleteMessages/bot_messages.py
@@ -0,0 +1,8 @@
+class ChatMSG(object):
+    START_TXT = """Hello.. <b>{} 👋</b>
+    
+<code>I can delete medias in Your Group!
+
+Just.. add me as as admin with all privileges in your Group !
+Then.. Send </code><b> /cleanmedia</b><code> to delete !</code>
+"""               
