4cf63cd79228923a2057de33b201b915d7e32672
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-29 22:41:18 +0530

    Create delete_messages.py

---
+++ DeleteMessages/helper/delete_messages.py
@@ -0,0 +1,10 @@
+from typing import List
+from DeleteMessagesBot.bot import DeleteBot as Bot
+
+
+async def mass_delete_messages(client: Bot, chat_id: int, message_ids: List[int]):
+    return await client.delete_messages(
+        chat_id=chat_id,
+        message_ids=message_ids,
+        revoke=True
+    
